9a8ea83757d7823bf3c4793e7c8a5deb767f785e
Timothy Merritt <timmybytes@gmail.com>
2021-10-12 22:17:50 -0700

    chore: Fix redundant Footer link

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -9,21 +9,19 @@ export const Footer = (): React.ReactElement => {
       w='100%'
       h={{ base: '80px', sm: '48px' }}
       py={4}>
-      <a href='https://timmybytes.com' target='_blank' rel='noreferrer'>
-        <Box d='flex' gridGap={2} justifyContent='center' alignItems='center'>
-          <Text whiteSpace='nowrap'>
-            Built by{' '}
-            <Text
-              as='a'
-              href='https://timmybytes.com'
-              target='_blank'
-              rel='noopener noreferrer'>
-              Timothy Merritt
-            </Text>
-            ; 2021
-          </Text>
-        </Box>
-      </a>
+      <Box d='flex' gridGap={2} justifyContent='center' alignItems='center'>
+        <Text whiteSpace='nowrap'>
+          Built by{' '}
+          <Text
+            as='a'
+            href='https://timmybytes.com'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Timothy Merritt
+          </Text>{' '}
+          2021
+        </Text>
+      </Box>
     </Box>
   )
 }
